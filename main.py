1856d589fb0a3975b0b7195a25ea3afc3c6a1739
Pups1k365 <151250376+Pups1k365@users.noreply.github.com>
2024-01-24 22:19:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-xyu
+import telebot
+
+TOKEN = '5942635111:AAHpSN0GqJ0CR8ERVSFf8H-tS3QYYRwZTok'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['привет'])
+def send_hello(message):
+    bot.reply_to(message, "пока")
+
+bot.polling()
