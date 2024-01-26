80754613aefc0cdefad695665a7b4acf96a9ede8
Pups1k365 <151250376+Pups1k365@users.noreply.github.com>
2024-01-26 19:16:36 +0200

    Update apistats.php

--- apistats.php
+++ apistats.php
@@ -2,12 +2,10 @@
 // URL изображения для сохранения
 $imageUrl = 'http://pups1k.000.pe/player_images/299J9L9PR_mico.png';
 
-// Имя файла, в котором будет сохранено изображение
-$fileName = 'saved_image.png';
+// Сохранение изображения в локальный файл
+$localImage = 'saved_image.png';
+file_put_contents($localImage, file_get_contents($imageUrl));
 
-// Сохранение изображения из указанного URL
-file_put_contents($fileName, file_get_contents($imageUrl));
-
-// Показ изображения
-echo '<img src="' . $fileName . '" alt="Saved Image">';
+// Отображение сохраненного изображения
+echo '<img src="' . $localImage . '" alt="Saved Image">';
 ?>
