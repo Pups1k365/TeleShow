85211f3a361299edd31c4a2d57996b96c2702a03
Pups1k365 <151250376+Pups1k365@users.noreply.github.com>
2024-01-30 18:03:00 +0200

    Update apistats.php

--- apistats.php
+++ apistats.php
@@ -1,12 +1,10 @@
 <?php
-  $playerId = $_GET['player_id'];
-  $imageType = $_GET['type'];
-  $imageUrl = "https://www.brawltime.ninja/api/render/profile/{$playerId}/{$imageType}.png?background=cartoon_lobby.jpg";
+  $imageUrl = "https://www.brawltime.ninja/api/render/profile/299J9L9PR/bibi.png?background=cartoon_lobby.jpg";
   
   // Скачиваем изображение и отправляем его для скачивания
   $imageData = file_get_contents($imageUrl);
   header('Content-Type: application/octet-stream');
   header("Content-Transfer-Encoding: Binary"); 
-  header("Content-disposition: attachment; filename=\"{$playerId}_{$imageType}.png\""); 
+  header("Content-disposition: attachment; filename=\"bibi_image.png\""); 
   echo $imageData;
 ?>
